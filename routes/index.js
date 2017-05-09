c26d5d138a1ea2cc0868c090fe4b0c48c783d1f9
kshaham <fedify8@gmail.com>
2017-05-08 20:56:04 -0400

    config

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CRUD Robot App' });
+  res.render('index', { title: 'ROBOT LIST APP' });
 });
 
 module.exports = router;
--- routes/robots.js
+++ routes/robots.js
@@ -12,59 +12,42 @@ if (true == false) {
 /* List Robots */
 
 router.get('/robots', function(req, res, next) {
-  var url = "https://southernct-443-robots-api.herokuapp.com/api/robots"
+  const endpointUrl = `${baseUrl}/api/robots`
 
-  fetch(url)
-    .then(function(response) {
-      response.json()
-        .then(function(json){
-          console.log("LISTING ROBOTS", json)
-          res.render('robots/index', {robots: json, title: "All Robots"});
-        })
+  fetch(endpointUrl).then(function(response) {
+    response.json().then(function(json){
+      console.log("LISTING ROBOTS", json.length)
+      res.render('robots/index', {robots: json, title: "Robot List"});
     })
-    .catch(function(err){
-      console.log("GOT AN ERROR:", err)
-      res.send({error: `OOPS - SERVER ERROR ${err}`});
-    })
+  })
 });
 
-/* New Robot */
+/* NEW */
 
-router.get('/robots/new', function(req, res, next) { // handle GET requests to the robots/new URL path
-  res.render('robots/new', { // render the robots/new.ejs view
-    title: "New Robot"
+router.get('/robots/new', function(req, res, next) { 
+  res.render('robots/new', {
+    title: "Add Robot"
   })
 })
 
 
-/* Show Robot */
+/* SHOW */
 
 router.get('/robots/:id', function(req, res, next) {
-  var robotId = req.params.id;
-  var errorMessage = `OOPS - COULDN'T FIND ROBOT ${robotId}`
+  const robotId = req.params.id
   const endpointUrl = `${baseUrl}/api/robots/${robotId}`
-  var url = `https://southernct-443-robots-api.herokuapp.com/api/robots/${robotId}`
-  //The same as the one above. Difference is called string interpolation^^. Bot is Concatination basically adding like in java
-  //var url = "https://southernct-443-robots-api.herokuapp.com/api/robots/" + robotID
 
-  fetch(url)
-    .then(function(response) {
-      response.json()
-        .then(function(json){
-          console.log("SHOWING ROBOT", json)
-          res.render('robots/show', {robot: json, title: `Robot ${robotId}`,requestUrl: endpointUrl});
-        })
-        .catch(function(err){
-          console.log("JSON PARSE ERROR", err)
-          res.send(errorMessage)
-        })
+  fetch(endpointUrl).then(function(response) {
+    response.json().then(function(json){
+      console.log("SHOWING THE ROBOT", json)
+      res.render('robots/show', {
+        robot: json,
+        title: `Robot: ${robotId}`,
+        requestUrl: endpointUrl
+      })
     })
-    .catch(function(err){
-      console.log(errorMessage)
-      res.send(errorMessage)
-    })
-});
-
+  })
+})
 
 /* Edit */
 router.get('/robots/:id/edit', function(req, res, next) {
